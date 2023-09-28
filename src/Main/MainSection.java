1cb5d9626abdb47a40e1a06ff93a17338a1f2a78
lin <lin496688@gmail.com>
2023-09-28 09:43:53 +0630

    added remote test

--- src/Main/MainSection.java
+++ src/Main/MainSection.java
@@ -4,6 +4,7 @@ public class MainSection {
 
 	public static void main(String[] args) {
 		System.out.println("Inside Main Method...");
-		System.out.println("Inside Main Method...");	
+		System.out.println("Inside Main Method...");
+		System.out.println("Test to push remote Master...");	
 	}
 }
